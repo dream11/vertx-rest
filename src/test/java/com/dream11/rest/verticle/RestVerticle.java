50001f671a9756a365f61764d6546f7f4bae6e4d
nikhilT-d11 <nikhiltummidi@gmail.com>
2024-03-28 16:36:00 +0530

    chore: add test for extra keys in request body

--- src/test/java/com/dream11/rest/verticle/RestVerticle.java
+++ src/test/java/com/dream11/rest/verticle/RestVerticle.java
@@ -6,7 +6,10 @@ import com.dream11.rest.ClassInjector;
 import com.dream11.rest.Constants;
 import com.dream11.rest.injector.GuiceInjector;
 import com.dream11.rest.util.SharedDataUtil;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.json.jackson.DatabindCodec;
 
 public class RestVerticle extends AbstractRestVerticle {
 
@@ -17,5 +20,10 @@ public class RestVerticle extends AbstractRestVerticle {
   @Override
   protected ClassInjector getInjector() {
     return SharedDataUtil.getInstance(this.vertx.getDelegate(), GuiceInjector.class);
+  }
+
+  @Override
+  protected ObjectMapper getMapper() {
+    return DatabindCodec.mapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
   }
 }
